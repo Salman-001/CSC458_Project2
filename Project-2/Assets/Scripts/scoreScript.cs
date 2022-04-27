04c2e90aa6d7babf1f9f196ccfe6d22413eacdc0
Mohamad <79073457+Salman-001@users.noreply.github.com>
2022-04-28 00:30:57 +0300

    final commit

--- Project-2/Assets/Scripts/scoreScript.cs
+++ Project-2/Assets/Scripts/scoreScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class scoreScript : MonoBehaviour
 {
@@ -22,6 +23,18 @@ public class scoreScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(score == 50){
+            if(SceneManager.GetActiveScene().buildIndex == 0){
+                SceneManager.LoadScene(1);
+            }else if(SceneManager.GetActiveScene().buildIndex == 1){
+                SceneManager.LoadScene(2);
+            }else if(SceneManager.GetActiveScene().buildIndex == 2){
+                SceneManager.LoadScene(3);
+            }else{
+                Application.Quit();
+            }
+        }
         
     }
 
